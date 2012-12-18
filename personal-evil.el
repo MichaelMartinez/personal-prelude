5f512ffa1fe59d694b1c6b084ab17f63b2bd433b
Michael Martinez <machinedesigner@gmail.com>
2012-12-18 14:59:26 -0700

    refinements to evil... ongoing (need to add evil-leader to packages)

--- personal-evil.el
+++ personal-evil.el
@@ -1,12 +1,23 @@
+(require 'evil)
 (evil-mode 1)
+(require 'evil-leader)
+(setq evil-leader/leader ","
+      evil-leader/in-all-states t)
+(setq evil-normal-state-tag (propertize "N" 'face '((:background "green" :foreground "black")))
+      evil-emacs-state-tag (propertize "E" 'face '((:background "orange" :foreground "black")))
+      evil-insert-state-tag (propertize "I" 'face '((:background "red")))
+      evil-motion-state-tag (propertize "M" 'face '((:background "blue")))
+      evil-visual-state-tag (propertize "V" 'face '((:background "grey80" :foreground "black")))
+      evil-operator-state-tag (propertize "O" 'face '((:background "purple"))))
+
 (define-key evil-normal-state-map "\C-e" 'move-end-of-line)
 (define-key evil-motion-state-map "\C-e" 'evil-end-of-line)
 (define-key evil-insert-state-map "\C-e" 'evil-end-of-line)
 
 
-(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+(define-key evil-insert-state-map "j" #'mam/maybe-exit)
 
-(evil-define-command cofi/maybe-exit ()
+(evil-define-command mam/maybe-exit ()
   :repeat change
   (interactive)
   (let ((modified (buffer-modified-p)))
