a0aa7b7e13c41688ab637acc6cef2a9b365f086e
Michael Martinez <machinedesigner@gmail.com>
2012-12-18 09:51:54 -0700

    change some evil

--- personal-evil.el
+++ personal-evil.el
@@ -1,14 +1,16 @@
 (evil-mode 1)
-(define-key evil-normal-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-normal-state-map "\C-e" 'move-end-of-line)
 (define-key evil-motion-state-map "\C-e" 'evil-end-of-line)
+(define-key evil-insert-state-map "\C-e" 'evil-end-of-line)
 
-(define-key evil-insert-state-map "k" #'cofi/maybe-exit)
+
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
 
 (evil-define-command cofi/maybe-exit ()
   :repeat change
   (interactive)
   (let ((modified (buffer-modified-p)))
-    (insert "k")
+    (insert "j")
     (let ((evt (read-event (format "Insert %c to exit insert state" ?j)
                nil 0.5)))
       (cond
