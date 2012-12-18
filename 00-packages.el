7480bfdfe2d3f403bc07a4842220d98132a2d593
Michael Martinez <machinedesigner@gmail.com>
2012-12-18 15:02:45 -0700

    add powerline to packages

--- 00-packages.el
+++ 00-packages.el
@@ -1,6 +1,7 @@
 ;; My packages
 (setq prelude-packages (append '(evil
                                  evil-leader
+                                 powerline
                                  cyberpunk-theme
                                  ) prelude-packages))
 
