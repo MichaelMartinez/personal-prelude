b53b6a7c8f22a1310094983cd1ffb94ffab3cbd4
Michael Martinez <machinedesigner@gmail.com>
2013-01-01 23:09:03 -0700

    remove items from erc in preperation for inclusion of prelude erc. changed global font to source code pro.

--- personal-global.el
+++ personal-global.el
@@ -7,7 +7,7 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
 ;; Set Global font to Source Code Pro
-(set-face-attribute 'default nil :font "DejaVu Sans Mono-14")
+(set-face-attribute 'default nil :font "Source Code Pro-14")
 
 ;; Set C-c C-m to M-x so if I miss x it still fires
 (global-set-key "\C-c\C-m" 'execute-extended-command)
@@ -18,28 +18,3 @@
 ;; Need to get this file into its own directory
 ;; as nxhtml is a giant beast that has tons of options
 (load "~/.emacs.d/vendor/nxhtml/autostart.el")
-
-;; None of these worked set python3 as interpreter
-;; running M-! which python returns: /usr/bin/python
-;; running M-! which python3 returns: /usr/local/bin/python3
-;;(setq python-python-command "/usr/local/bin/python3")
-;;(setenv "PYTHONPATH" "/usr/local/bin/python3")
-
-
-;; You can also do as I do and keep them in a seperate folder:
-(add-to-list 'load-path "~/.emacs.private")
-;;Load the password file but don't complain if it doesn't exist
-;; use 'noerror inside parens once its loaded
-(load "real-passwords" 'noerror)
-
-;; These are sample passwords for a simple private file
-
-;;Bip server
-;;(setq bip-username "YourBipUsername")
-;;(setq bip-password "Username:Password:Network")
-;;Twitter
-;;(setq twit-user "YourTwitterUsername")
-;;(setq twit-pass "YourTwitterPassword")
-;;Gmail - don't forget to setup a site specific pass for emacs gnus
-;;(setq gmail-user "YourGmailUsername")
-;;(setq gmail-pass "YourGmailPassword")
