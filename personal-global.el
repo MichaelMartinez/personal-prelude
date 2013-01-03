2fed2a28c7778b4af4c733682e48e55b230ace5d
Michael Martinez <machinedesigner@gmail.com>
2013-01-02 18:01:48 -0700

    change global to reflect nxhtml move to personal

--- personal-global.el
+++ personal-global.el
@@ -17,6 +17,6 @@
 
 ;; Need to get this file into its own directory
 ;; as nxhtml is a giant beast that has tons of options
-(load "~/.emacs.d/vendor/nxhtml/autostart.el")
+(load "~/.emacs.d/personal/vendor/nxhtml/autostart.el")
 
 (provide 'personal-global)
