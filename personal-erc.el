934bd7a6d873ddb0d56dbae2f89ed5cc6460595c
Michael Martinez <machinedesigner@gmail.com>
2013-01-05 23:02:36 -0700

    change keybindings in evil, remove erc verification until further notice and add snippets and nxhtml as submodules

--- personal-erc.el
+++ personal-erc.el
@@ -1,8 +1,8 @@
-(load "~/.emacs.private")
-(require 'erc-services)
-(erc-services-mode 1)
-(setq erc-prompt-for-nickserv-password nil)
-(setq erc-nickserv-passwords
-      `((freenode     (("michaelmartinez" . ,freenode-nickone-pass)))
-        ))
-(provide 'personal-erc)
+;; (load "~/.emacs.private")
+;; (require 'erc-services)
+;; (erc-services-mode 1)
+;; (setq erc-prompt-for-nickserv-password nil)
+;; (setq erc-nickserv-passwords
+;;       `((freenode     (("michaelmartinez" . ,freenode-nickone-pass)))
+;;         ))
+;; (provide 'personal-erc)
--- personal-evil.el
+++ personal-evil.el
@@ -1,8 +1,5 @@
 (require 'evil)
 (evil-mode 1)
-(require 'evil-leader)
-(setq evil-leader/leader ","
-      evil-leader/in-all-states t)
 (setq evil-normal-state-tag (propertize "N" 'face '((:background "green" :foreground "black")))
       evil-emacs-state-tag (propertize "E" 'face '((:background "orange" :foreground "black")))
       evil-insert-state-tag (propertize "I" 'face '((:background "red")))
@@ -16,47 +13,6 @@
 (define-key evil-normal-state-map "\C-e" 'move-end-of-line)
 (define-key evil-motion-state-map "\C-e" 'evil-end-of-line)
 (define-key evil-insert-state-map "\C-e" 'evil-end-of-line)
-
-;; Use s-[h, j, k, l] for window navigation
-(define-key evil-normal-state-map "\C-h" 'windmove-left) ;; move left around split
-(define-key evil-normal-state-map "\C-l" 'windmove-right) ;; move right around split
-;; Use C-j and C-k for scrolling 5 lines up/down
-(define-evil-motion-key (read-kbd-macro "C-j") (lambda () (interactive) (evil-scroll-line-down 5) (evil-next-line 5))) (define-evil-motion-key (read-kbd-macro "C-k") (lambda () (interactive) (evil-scroll-line-up 5) (evil-previous-line 5)))
-
-;; Evil Leader stuff
- (evil-leader/set-key
-;;   "e" '
-;;   "E" '
-;;   "o" '
-;;   "O" '
-   "b" 'prelude-rename-file-and-buffer
-;;   "B" '
-   "G" 'prelude-google
-   "w" 'save-buffer
-   "W" 'save-some-buffers
-   "k" 'kill-current-buffer
-   "K" 'kill-buffer-and-window
-;;   "<" '
-   "d" 'dired-jump
-   "D" 'prelude-duplicate-current-line-or-region
-   "I" 'prelude-indent-blockquote-and-copy-to-clipboard
-   "i" 'prelude-indent-region-or-buffer
-   "m" 'compile
-
-   "n" 'split-window-horizontally
-   "c" 'delete-window
-   "N" 'make-frame-command
-   "C" 'delete-frame
-
-   "g" 'magit-status
-;;   "h" '
-
-;;   "s" '
-;;   ";" '
-
-;;   "." 'evil-ex
-   )
-
 
 (define-key evil-insert-state-map "j" #'mam/maybe-exit)
 
